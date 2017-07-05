c2c073e8780f209c5a7b5989173a18c551e9d71f
Mister Oyster <oysterized@gmail.com>
2017-07-05 14:56:20 +0200

    vt: add a patches folder and install script

---
+++ patches/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs=""
+
+# red + nocolor
+RED='\033[0;31m'
+NC='\033[0m'
+
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+        echo -e "\n${RED}Applying ${NC}$dir ${RED}patches...${NC}\n"
+	#git am $rootdirectory/vendor/mediatek/patches/$dir/*.patch
+	git apply -v $rootdirectory/vendor/mediatek/patches/$dir/*.patch
+done
+
+# -----------------------------------
+echo -e "Done !\n"
+cd $rootdirectory
+
