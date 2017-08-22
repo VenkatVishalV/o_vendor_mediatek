a21f439ff1ae008e2c47bfa63fe6ddc2f1c9734e
Mister Oyster <oysterized@gmail.com>
2017-08-22 18:57:08 +0200

    tree: cleanup patches already in los-15 https://github.com/LineageOS/android_bionic/commit/a091cb1296b417c66c84850a2b02f586bc85c894

--- patches/install.sh
+++ patches/install.sh
@@ -4,7 +4,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic build/make/core build/soong system/core vendor/lineage"
+dirs="build/make/core build/soong system/core vendor/lineage"
 
 # red + nocolor
 RED='\033[0;31m'
