781631448df7c2ef0198eda3639676346c20764e
Mister Oyster <oysterized@gmail.com>
2017-10-20 18:45:13 +0200

    patch: add missing commit to include fd_utils-inl-extra headers
    see :
    https://github.com/Moyster/nougat_device_meizu_m2note/blob/los-14.1/include/fd_utils-inl-extra.h

--- patches/install.sh
+++ patches/install.sh
@@ -4,7 +4,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="build/make/core build/soong system/core"
+dirs="build/make/core build/soong frameworks/base system/core"
 
 # red + nocolor
 RED='\033[0;31m'
