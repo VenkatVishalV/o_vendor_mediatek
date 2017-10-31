dc1807f11b565304e08b99d80da215fc9a435a08
Mister Oyster <oysterized@gmail.com>
2017-10-31 16:23:59 +0100

    hal: add and build CM power hal
    built and moved to /vendor as expected by Oreo
    
    git history available here :
    https://github.com/Moyster/nougat_device_meizu_m2note/commits/los-14.1/mtk/power

---
+++ VendorProduct.mk
@@ -0,0 +1,8 @@
+# CM mediatek powerHAL
+PRODUCT_PACKAGES += \
+    power.default \
+    power.mt6753 \
+    vendor.lineage.power@1.0-impl
+    #android.hardware.power@1.0-impl // non-lineage imp
+
+#TARGET_TAP_TO_WAKE_NODE=
